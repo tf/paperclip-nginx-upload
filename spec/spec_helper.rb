65f565e7f883d9cacf32520240c1450bb2a342aa
Tim Fischbach <mail@timfischbach.de>
2017-07-28 15:54:53 +0200

    Fix test suite for Paperclip 5.1
    Ensure Active Support `delegate`, which is used by paperclip, is
    present.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'rspec'
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')).freeze
 $LOAD_PATH << File.join(PROJECT_ROOT, 'lib')
 
+require 'active_support/core_ext/module'
 require 'paperclip/nginx/upload'
 
 Dir[File.join(PROJECT_ROOT, 'spec', 'support', '**', '*.rb')].each { |file| require(file) }
